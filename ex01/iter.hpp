ea950b3d0981cdbc4daad5d987c5aafdcf9f0f88
Caglar <caglarnal@hotmail.com>
2024-06-14 03:50:44 +0200

    refactor: Update iter.hpp to use const T& in iter function

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -4,7 +4,7 @@
 #include <cstddef>
 
 template <typename T>
-void iter(T* arr, std::size_t len, void (*func)(T&))
+void iter(T* arr, std::size_t len, void (*func)(const T&))
 {
 	for (std::size_t i = 0; i < len; i++)
 		func(arr[i]);
