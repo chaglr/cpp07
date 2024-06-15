8aabe9755d6d75a032857a206f734446b2816827
Caglar <caglarnal@hotmail.com>
2024-06-16 01:20:46 +0200

    changing the intent type

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -8,82 +8,82 @@
 template <typename T>
 class Array {
 private:
-    T* data;
-    std::size_t length;
+	T* data;
+	std::size_t length;
 
 public:
-    // Default constructor
-    Array() : data(nullptr), length(0) {}
+	// Default constructor
+	Array() : data(nullptr), length(0) {}
 
-    // Constructor with size
-    explicit Array(std::size_t n) : data(nullptr), length(n) {
-        if (n > 0) {
-            data = new T[n]();
-        }
-    }
+	// Constructor with size
+	explicit Array(std::size_t n) : data(nullptr), length(n) {
+		if (n > 0) {
+			data = new T[n]();
+		}
+	}
 
-    // Copy constructor
-    Array(const Array& other) : data(nullptr), length(other.length) {
-        if (length > 0) {
-            data = new T[length];
-            for (std::size_t i = 0; i < length; ++i) {
-                data[i] = other.data[i];
-            }
-        }
-    }
+	// Copy constructor
+	Array(const Array& other) : data(nullptr), length(other.length) {
+		if (length > 0) {
+			data = new T[length];
+			for (std::size_t i = 0; i < length; ++i) {
+				data[i] = other.data[i];
+			}
+		}
+	}
 
-    // Copy assignment operator
-    Array& operator=(const Array& other) {
-        if (this != &other) {
-            T* newData = other.length > 0 ? new T[other.length] : nullptr;
-            for (std::size_t i = 0; i < other.length; ++i) {
-                newData[i] = other.data[i];
-            }
-            delete[] data;
-            data = newData;
-            length = other.length;
-        }
-        return *this;
-    }
+	// Copy assignment operator
+	Array& operator=(const Array& other) {
+		if (this != &other) {
+			T* newData = other.length > 0 ? new T[other.length] : nullptr;
+			for (std::size_t i = 0; i < other.length; ++i) {
+				newData[i] = other.data[i];
+			}
+			delete[] data;
+			data = newData;
+			length = other.length;
+		}
+		return *this;
+	}
 
-    // Destructor
-    ~Array() {
-        delete[] data;
-    }
+	// Destructor
+	~Array() {
+		delete[] data;
+	}
 
-    // Access operator
-    T& operator[](std::size_t index) {
-        if (index >= length) {
-            throw std::out_of_range("Index out of bounds");
-        }
-        return data[index];
-    }
+	// Access operator
+	T& operator[](std::size_t index) {
+		if (index >= length) {
+			throw std::out_of_range("Index out of bounds");
+		}
+		return data[index];
+	}
 
-    const T& operator[](std::size_t index) const {
-        if (index >= length) {
-            throw std::out_of_range("Index out of bounds");
-        }
-        return data[index];
-    }
+	const T& operator[](std::size_t index) const {
+		if (index >= length) {
+			throw std::out_of_range("Index out of bounds");
+		}
+		return data[index];
+	}
 
-    // Size method
-    std::size_t size() const {
-        return length;
-    }
+	// Size method
+	std::size_t size() const {
+		return length;
+	}
 };
 
 // Overloading the output operator for Array class
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const Array<T>& array) {
-    os << "[";
-    for (std::size_t i = 0; i < array.size(); ++i) {
-        os << array[i];
-        if (i < array.size() - 1) {
-            os << ", ";
-        }
-    }
-    os << "]";
-    return os;
+	os << "[";
+	for (std::size_t i = 0; i < array.size(); ++i) {
+		os << array[i];
+		if (i < array.size() - 1) {
+			os << ", ";
+		}
+	}
+	os << "]";
+	return os;
 }
 
 #endif // ARRAY_HPP
--- ex02/Test.hpp
+++ ex02/Test.hpp
@@ -6,36 +6,36 @@
 
 class Test {
 public:
-    int value;
+	int value;
 
-    // Default constructor
-    Test(int val = 0) : value(val) {
-        std::cout << "Constructing Test with value " << value << std::endl;
-    }
+	// Default constructor
+	Test(int val = 0) : value(val) {
+		std::cout << "Constructing Test with value " << value << std::endl;
+	}
 
-    // Copy constructor
-    Test(const Test& other) : value(other.value) {
-        std::cout << "Copying Test with value " << value << std::endl;
-    }
+	// Copy constructor
+	Test(const Test& other) : value(other.value) {
+		std::cout << "Copying Test with value " << value << std::endl;
+	}
 
-    // Assignment operator
-    Test& operator=(const Test& other) {
-        if (this != &other) {
-            std::cout << "Assigning Test with value " << other.value << std::endl;
-            value = other.value;
-        }
-        return *this;
-    }
+	// Assignment operator
+	Test& operator=(const Test& other) {
+		if (this != &other) {
+			std::cout << "Assigning Test with value " << other.value << std::endl;
+			value = other.value;
+		}
+		return *this;
+	}
 
-    // Destructor
-    ~Test() {
-        std::cout << "Destroying Test with value " << value << std::endl;
-    }
+	// Destructor
+	~Test() {
+		std::cout << "Destroying Test with value " << value << std::endl;
+	}
 
-    // String representation
-    std::string toString() const {
-        return "Test(" + std::to_string(value) + ")";
-    }
+	// String representation
+	std::string toString() const {
+		return "Test(" + std::to_string(value) + ")";
+	}
 };
 
 #endif // TEST_HPP
